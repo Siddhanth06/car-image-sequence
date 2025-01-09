7d37416d931e39f3800f50e7251a46fdb6ca9d41
Siddhanth0629 <147157031+Siddhanth0629@users.noreply.github.com>
2025-01-09 17:23:23 +0530

    observer-scroll

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
+  gsap.registerPlugin(ScrollTrigger);
+  gsap.registerPlugin(Observer);
+
   CustomEase.create("hop", "M0,0 C0.355,0.022 0.488,0.079 0.5,0.5 0.542,0.846 0.615,1,1,1");
 
   CustomEase.create("hop2", "M0,0 C0.078,0.617 0.114,0.716 0.255,0.828 0.373,0.922 0.561,1,1,1");
@@ -54,7 +57,7 @@ document.addEventListener("DOMContentLoaded", () => {
         duration: 1.25,
         ease: "power3.out",
       },
-      ">-0.5"
+      ">-0.7"
     );
   });
 
@@ -105,4 +108,173 @@ document.addEventListener("DOMContentLoaded", () => {
       // Enable scrolling when the timeline completes
       document.body.style.overflow = "";
     });
+
+  // document.querySelectorAll(".section").forEach((sectionElement, index) => {
+  //   if (index === sections.length - 1) {
+  //     return; // Skip the last section
+  //   }
+  //   const containerElement = sectionElement.querySelector(".container");
+  //   const nextSectionElement = sectionElement.parentNode.querySelector(
+  //     `section:nth-child(${index + 2})`
+  //   );
+
+  //   gsap
+  //     .timeline({
+  //       scrollTrigger: {
+  //         trigger: sectionElement,
+  //         start: `center (nextSectionElement.offsetTop - (window.innerHeight * 0.5))`,
+  //         end: `bottom 20%`,
+  //         scrub: 1,
+  //         pin: true,
+  //         pinSpacing: false,
+  //         markers: true,
+  //       },
+  //     })
+  //     .to(containerElement, {
+  //       opacity: 0,
+  //       scale: 0.8,
+  //       yPercent: 30,
+  //       duration: 3,
+  //       ease: "power1.out",
+  //     });
+  // });
+
+  //Move page to top on refresh
+
+  document.querySelectorAll(".section").forEach((sectionElement, index, sections) => {
+    // Check if it's the last section
+    if (index === sections.length - 1) {
+      return; // Skip the last section
+    }
+
+    const containerElement = sectionElement.querySelector(".container");
+    const nextSectionElement = sectionElement.parentNode.querySelector(
+      `section:nth-child(${index + 2})`
+    );
+
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: sectionElement,
+          start: `center (nextSectionElement.offsetTop - (window.innerHeight * 0.5))`,
+          end: `bottom 20%`,
+          scrub: 1,
+          pin: true,
+          pinSpacing: false,
+          markers: true,
+        },
+      })
+      .to(containerElement, {
+        opacity: 0,
+        scale: 0.8,
+        yPercent: 30,
+        duration: 3,
+        ease: "power1.out",
+      });
+  });
+
+  window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+  };
+
+  //Lenis scroll
+  // Initialize Lenis
+  const lenis = new Lenis({
+    duration: 2, // Set the smooth scrolling duration
+    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // Default linear easing function
+    smooth: true, // Enable smooth scrolling
+    direction: "vertical", // Scroll direction ('vertical' or 'horizontal')
+  });
+
+  // Update Lenis on every animation frame
+  function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+  }
+
+  requestAnimationFrame(raf);
+
+  // Sync Lenis with GSAP ScrollTrigger
+  gsap.ticker.add((time) => {
+    lenis.raf(time * 1000);
+  });
+
+  //Greensock horizontal scroll
+  gsap.to(
+    ".scroll-section",
+    {
+      position: "fixed",
+      top: 0,
+      // visibility: "hidden",
+      scrollTrigger: {
+        trigger: ".first",
+        start: "top top",
+        bottom: "bottom bottom",
+        scrub: true,
+        markers: true,
+      },
+    },
+    "abc"
+  );
+
+  gsap.to(".bg", { top: 0 }, "abc");
+
+  gsap.registerPlugin(Observer);
+
+  let sections = document.querySelectorAll(".scroll-section"),
+    images2 = document.querySelectorAll(".bg"),
+    headings = gsap.utils.toArray(".section-heading"),
+    outerWrappers = gsap.utils.toArray(".outer"),
+    innerWrappers = gsap.utils.toArray(".inner"),
+    currentIndex = -1,
+    wrap = gsap.utils.wrap(0, sections.length),
+    animating;
+
+  gsap.set(outerWrappers, { yPercent: 100 });
+  gsap.set(innerWrappers, { yPercent: -100 });
+
+  function gotoSection(index, direction) {
+    index = wrap(index); // make sure it's valid
+    animating = true;
+    let fromTop = direction === -1,
+      dFactor = fromTop ? -1 : 1,
+      tl = gsap.timeline({
+        defaults: { duration: 1.25, ease: "power1.inOut" },
+        onComplete: () => (animating = false),
+      });
+    if (currentIndex >= 0) {
+      // The first time this function runs, current is -1
+      gsap.set(sections[currentIndex], { zIndex: 0 });
+      tl.to(images2[currentIndex], { yPercent: -15 * dFactor }).set(sections[currentIndex], {
+        autoAlpha: 0,
+      });
+    }
+    gsap.set(sections[index], { autoAlpha: 1, zIndex: 1 });
+    tl.fromTo(
+      [outerWrappers[index], innerWrappers[index]],
+      {
+        yPercent: (i) => (i ? -100 * dFactor : 100 * dFactor),
+      },
+      {
+        yPercent: 0,
+      },
+      0
+    ).fromTo(images2[index], { yPercent: 15 * dFactor }, { yPercent: 0 }, 0);
+
+    currentIndex = index;
+  }
+
+  Observer.create({
+    type: "wheel,touch,pointer",
+    wheelSpeed: -1,
+    onDown: () => !animating && gotoSection(currentIndex - 1, -1),
+    onUp: () => !animating && gotoSection(currentIndex + 1, 1),
+    tolerance: 10,
+    preventDefault: true,
+  });
+
+  gotoSection(0, 1);
+
+  // original: https://codepen.io/BrianCross/pen/PoWapLP
+  // horizontal version: https://codepen.io/GreenSock/pen/xxWdeMK
 });
